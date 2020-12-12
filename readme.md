e36a8fbc5c034b99460613360d5aa8348c6ea151
张博奕 <zhangboyi@xiaomi.com>
2020-12-12 10:29:39 +0800

    demo

---
+++ readme.md
@@ -0,0 +1,5 @@
++ 与 `chat-client` 位于同一根目录下
++ 在 `chat-client` 中 `npm i & npm run build`
++ `npm i & npm run serve`
++ 访问 `http://localhost:3333/index.html`
++ 选择身份，开始畅聊...
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Socket, SocketType } from "dgram";
+const path = require('path')
 
 const express = require('express');
 const socket = require('socket.io');
@@ -7,19 +7,51 @@ const app = express();
 const server = require('http').createServer(app);
 const io = socket(server);
 
+app.use(express.static(path.join(__dirname, '../../chat-client/dist')))
+
 server.listen(3333, () => {
   console.log('server...', 3333);
 });
 
+interface Message {
+  id: number,
+  to_id: number,
+  from_id: number,
+  content: string,
+  create_time: number,
+}
+
+const sockets:{[propName: string]: any} = {}
+const mapId2Socket:{[propName: string]: string} = {}
+const msgs:{[propName: string]: Message} = {}
+
 
 io.on('connection', (socket: any) => {
   console.log('connection', socket.id)
+  socket.on('init_link', (data:any) => {
+    mapId2Socket[String(data.id)] = socket.id
+    sockets[socket.id] = socket
+  })
   socket.on('send_msg', (data: any) => {
-    console.log(data)
+    const { to_id, from_id, content, create_time } = data
+    const msg_id = `${new Date().getTime()}${from_id}${to_id}`
+    const msg = {
+      ...data,
+      id: Number(msg_id)
+    }
+    msgs[msg_id] = msg
+    if (mapId2Socket[to_id] && sockets[mapId2Socket[to_id]]) {
+      console.log('通知接收方')
+      sockets[mapId2Socket[to_id]].emit('recv_msg', msg)
+    }
+    if (mapId2Socket[from_id] && sockets[mapId2Socket[from_id]]) {
+      console.log('通知发送方')
+      sockets[mapId2Socket[from_id]].emit('recv_msg', msg)
+    }
   });
-  socket.send('recv_msg', {
-    data: '666'
-  });
+  // socket.emit('recv_msg', {
+  //   data: '666'
+  // });
 });
 
 
